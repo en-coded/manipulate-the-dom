d62163807e80d9cdb0992d378e83e00d8259e30f
en-coded <83879346+en-coded@users.noreply.github.com>
2021-05-11 22:46:59 -0400

    Part 5

--- script.js
+++ script.js
@@ -13,23 +13,40 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
   };
   randomQuote();
-  
-  // Do all of your work inside the document.addEventListener  
+
+  // Do all of your work inside the document.addEventListener
 
   // Part 1
 
+const updateText = document.querySelector("#main-title");
+updateText.textContent = "Hello World";
 
   // Part 2
 
+const updateBackColor = document.querySelector("body");
+updateBackColor.style.backgroundColor = "lightblue";
 
   // Part 3
 
+var elem = document.getElementById("favorite-things");
+elem.removeChild(elem.lastElementChild);
 
   // Part 4
 
+var x = document.getElementsByClassName("special-title");
+var i;
+for (i = 0; i < x.length; i++) {
+  x[i].style.fontsize = "2rem";
+}
+
+const specTitle = document.querySelectorAll(".special-title");
+specTitle.forEach(function(title) {
+  title.style.fontsize = "2rem";
+});
 
   // Part 5
 
+var races = document.getElementById("favorite-things");
 
   // Part 6
 
